29e4d88665d73a4fdb657490cc330cb4884e2020
ChaseManseth <chasemanseth@gmail.com>
2020-02-09 15:28:33 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const Data = require('./data');
 var authRoutes = require('./routes/auth');
 
 
-const API_PORT = process.end.PORT || 3001;
+const API_PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 const router = express.Router();
